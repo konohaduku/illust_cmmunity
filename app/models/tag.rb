8244581fdc032aa7aa17290b92b8b2e3612af72a
konohaduku <129601109+konohaduku@users.noreply.github.com>
2023-06-27 01:58:03 +0000

    [depp]

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -3,5 +3,5 @@ class Tag < ApplicationRecord
   # タグは複数の投稿を持つ　それは、post_tagsを通じて参照できる
   has_many :illusts,through: :post_tags
 
-  validates :name, uniqueness: true, presence: true
+  validates :tag_list, uniqueness: true, presence: true
 end
