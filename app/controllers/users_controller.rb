e4a07276d48acc3b0216cfa92380efef34f11a97
konohaduku <129601109+konohaduku@users.noreply.github.com>
2023-06-27 12:04:04 +0000

    test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :ensure_correct_user, only: [:edit, :update]
-  # before_action :ensure_guest_user, only: [:edit]
+  before_action :ensure_guest_user, only: [:edit]
   # before_action :admin_user,     only: [:destroy]
 
   def index
